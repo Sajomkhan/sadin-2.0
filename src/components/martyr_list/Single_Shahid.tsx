9eeb4cd8f223fe7e0489ecbc23a586792efd5034
Saiful Ajom Khan <sajomkhan@gmail.com>
2024-08-31 09:30:47 -0700

    Updated

--- src/components/martyr_list/Single_Shahid.tsx
+++ src/components/martyr_list/Single_Shahid.tsx
@@ -43,7 +43,44 @@ const Single_Shahid = () => {
               <p>{data.death_place}</p>
             </div>
           </div>
-          <div className="h-screen bg-blue-300"></div>
+          <div className="h-screen">
+            <p className="text-lg font-semibold mb-7">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+              recusandae magni laborum incidunt sit quos labore iusto aperiam
+              velit deserunt maxime!
+            </p>
+            <div className="whitespace-pre-wrap">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+              veritatis dolores, possimus sed perspiciatis rem deleniti
+              distinctio itaque neque dicta aliquam ad velit eveniet
+              exercitationem eaque reiciendis! Cupiditate perspiciatis quos
+              aspernatur consequatur iure molestiae consectetur dolores, nam non
+              sequi illum doloremque laboriosam maxime veniam soluta modi
+              distinctio in sapiente provident laudantium dolorum quas qui
+              deleniti? 
+              
+              Earum dignissimos illum dicta maiores aspernatur!
+              Quisquam quaerat placeat quod dolor reprehenderit in sequi
+              praesentium. Soluta laborum quis temporibus earum. Mollitia
+              maiores optio ducimus quisquam voluptate in excepturi ea quam
+              dignissimos fugit itaque, sed ut fuga accusamus architecto culpa
+              pariatur animi aperiam odit recusandae. Maiores, libero delectus
+              repellat molestias sint harum labore sit corporis accusantium qui
+              beatae quae nesciunt reiciendis dolorum porro dolores sequi ipsa
+              doloribus autem accusamus totam quod. Esse, a ipsa dolorum
+              perferendis vero facilis amet tempora voluptatem fugiat error?
+
+
+              Neque suscipit rerum consequatur rem asperiores tempora incidunt
+              cumque, alias est beatae sint atque perferendis explicabo magnam
+              maxime magni iure recusandae id nihil qui quae iusto quidem,
+              inventore debitis. Veniam quis molestiae aut similique?
+              Reprehenderit alias quas qui tempora rerum sapiente eum suscipit,
+              tenetur ipsam fugiat nesciunt recusandae atque, totam fuga odio.
+              Voluptatum suscipit odio obcaecati ab rem libero odit excepturi
+              temporibus facere.
+            </div>
+          </div>
         </div>
       </div>
     </div>
